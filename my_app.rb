23b4bc007562e537f4fcf265f1b1681387a4f4cb
Bo Mattsson <bo.mattsson@cint.com>
2015-09-16 12:54:10 +0100

    forgot the view folder....

--- my_app.rb
+++ my_app.rb
@@ -1,14 +1,9 @@
 require 'sinatra'
 
 get '/' do 
-   "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb :index
 end
 
-get '/secret' do
-  'Och den här är inte särskilt hemlig'
-end
 
 
 
